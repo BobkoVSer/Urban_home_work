99167c76361d8bd4905bf56b23c7be40b546d9a3
Bobkov_SV <sergio402@mail.ru>
2024-01-11 19:35:11 +0700

    work_2

---
+++ probe/pythonProject3/birk.py
@@ -0,0 +1,13 @@
+my_string='brother'
+print('my_string')
+'brother'[2:5]
+print('brother')
+another_string='sister'
+print ('sister')
+print('my_string'+'another_string')
+print ('brother'+'sister')
+print ('brother'[1])
+print('brother'[2:5])
+print('brother'[3:5])
+print('brother'[-3:-5])
+print(len('brother'))
