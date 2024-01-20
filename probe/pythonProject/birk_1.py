8a85dfddce7bad5c37e8b7220a83423a56e2c12b
BobkoVSer <156786477+BobkoVSer@users.noreply.github.com>
2024-01-20 11:46:11 +0700

    work1

---
+++ probe/pythonProject/birk_1.py
@@ -0,0 +1,10 @@
+my_list = ['апельсин', 'арбуз', 'банан']
+print(my_list)
+print(my_list [-1])
+print(my_list [0])
+my_list[2]="яблоко"
+print(my_list)
+my_dict = {'апельсин':'orange', 'banana': 'банан', 'apple': 'яблоко'}
+print(my_dict)
+my_dict['tomato']='помидор'
+print(my_dict)
