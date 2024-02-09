f214f959f932cc8e8872b9b515bfad2b002306d3
BobkoVSer <156786477+BobkoVSer@users.noreply.github.com>
2024-02-09 22:54:07 +0700

    Add files via upload

--- probe_4_6.py
+++ probe_4_6.py
@@ -1,12 +1,12 @@
 def test():
+    def test_2():
+        x, y, z  = 33, 44, 55
+        print(x, y, z)
+    test_2()
     a, b = 11, 22
     print(a, b)
 test()
 
-def test_2():
-    d, e, f = 33, 44, 55
-    print(f)
-    print(e)
-    print(d)
-test_2()
+
+
 
